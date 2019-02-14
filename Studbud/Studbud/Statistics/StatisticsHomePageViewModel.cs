b6547e1689465ca93bd570e51ad51000731f8e3c
Jingchao Feng <fjch1997@outlook.com>
2019-02-14 17:12:20 -0500

    Microcharts statisticsHomePage

--- Studbud/Studbud/Statistics/StatisticsHomePageViewModel.cs
+++ Studbud/Studbud/Statistics/StatisticsHomePageViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Studbud.External;
+﻿using Microcharts.Forms;
+using Studbud.External;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using Microcharts;
 
 namespace Studbud.Statistics
 {
@@ -10,12 +12,18 @@ namespace Studbud.Statistics
         public INavigationService NavigationSerive { get; set; }
 
         public ICommand OpenTimelineCommand { get; set; }
+        public ChartView ChartView { get; set; }
         public StatisticsHomePageViewModel()
         {
             OpenTimelineCommand = new DelegateCommand(() =>
             {
                 NavigationSerive.PushAsync(new TimelinePage());
             });
+            InitializeChart();
+        }
+        private void InitializeChart()
+        {
+            ChartView.Chart = new BarChart(); // TODO: This should be a pie chart here. See Microchart's documentation to find what to call.
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
