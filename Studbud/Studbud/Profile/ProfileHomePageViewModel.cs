98494e35f12840ead371bb942e98feb328f11d42
Jingchao Feng <fjch1997@outlook.com>
2019-02-28 15:48:17 -0500

    fixed

--- Studbud/Studbud/Profile/ProfileHomePageViewModel.cs
+++ Studbud/Studbud/Profile/ProfileHomePageViewModel.cs
@@ -9,16 +9,13 @@ using System.Windows.Input;
 
 namespace Studbud.Profile
 {
-    public class ProfileHomePageViewModel : INotifyPropertyChanged
+    public class ProfileHomePageViewModel : INotifyPropertyChanged, ISupportInitialize
     {
         public ITransactionStorageService TransactionStorageService { get; set; }
         public IAuthenticationService AuthenticationService { get; set; }
         public INavigationService NavigationService { get; set; }
         public ProfileHomePageViewModel()
         {
-            var startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
-            var endTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, 1).AddDays(-1);
-            spent = TransactionStorageService.GetTransactions(startTime, endTime).Sum(t => t.Amount);
             LogoutCommand = new DelegateCommand(() =>
             {
                 AuthenticationService.Logout();
@@ -30,15 +27,22 @@ namespace Studbud.Profile
         public decimal Budget { get => budget; set { budget = value; OnPropertyChanged(); OnPropertyChanged(nameof(Savings)); } }
         private decimal budget;
         public decimal Spent { get => spent; set { spent = value; OnPropertyChanged(); } }
-
-   
-
         private decimal spent;
 
         public DelegateCommand LogoutCommand { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        
+
+        public void BeginInit()
+        {
+        }
+
+        public void EndInit()
+        {
+            var startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            var endTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, 1).AddDays(-1);
+            spent = TransactionStorageService.GetTransactions(startTime, endTime).Sum(t => t.Amount);
+        }
     }
 }
